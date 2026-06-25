e9666c7ba5b1c132ac066d0f586f697bc6aa5179
Roman Ruiz-Esparza <2654709+rjruizes@users.noreply.github.com>
2026-06-25 12:57:59 -0400

    0.2.6: `kptn.task(requires=[])`

--- kptn/__init__.py
+++ kptn/__init__.py
@@ -1,14 +1,27 @@
 from __future__ import annotations
 
 # kptn package
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 # v0.2.0 public API
-from kptn.graph.decorators import task, sql_task, r_task, noop  # noqa: F401
-from kptn.graph.composition import parallel, Stage, map  # noqa: F401
-from kptn.graph.requires import any_of  # noqa: F401
-from kptn.graph.pipeline import Pipeline  # noqa: F401
+from kptn.graph.composition import Stage, map, parallel  # noqa: F401
 from kptn.graph.config import config  # noqa: F401
-from kptn.runner.api import run, plan  # noqa: F401
+from kptn.graph.decorators import noop, r_task, sql_task, task  # noqa: F401
+from kptn.graph.pipeline import Pipeline  # noqa: F401
+from kptn.graph.requires import any_of  # noqa: F401
+from kptn.runner.api import plan, run  # noqa: F401
 
-__all__ = ["task", "sql_task", "r_task", "noop", "parallel", "Stage", "map", "any_of", "Pipeline", "config", "run", "plan"]
+__all__ = [
+    "task",
+    "sql_task",
+    "r_task",
+    "noop",
+    "parallel",
+    "Stage",
+    "map",
+    "any_of",
+    "Pipeline",
+    "config",
+    "run",
+    "plan",
+]
